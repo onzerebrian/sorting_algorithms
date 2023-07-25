3154a04a88c435ea3b9629fdc7a3e13e50c09a38
onzerebrian <onzerebrian@gmail.com>
2023-07-25 17:00:25 +0300

    task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,5 +1,19 @@
 #include "sort.h"
 
+
+/**
+ * swap_integers - function that swaps two integers
+ * @a: First integer
+ * @b: Second integer
+ */
+void swap_integers(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
 /**
  * selection_sort - function
  * @array: array
@@ -8,7 +22,30 @@
 void selection_sort(int *array, size_t size)
 {
 
+	size_t i, j, index;
+	int min;
 
+	if (!array || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = array[i];
+		index = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < min)
+			{
+				min = array[j];
+				index = j;
+			}
+		}
+		if (index != i)
+		{
+			swap_integers(array + i, array + index);
+			print_array((const int *)array, size);
+		}
+	}
 
 
 }
