f6ad3f47c01fa53b9ad44698824e3dfeb6002e0f
onzerebrian <onzerebrian@gmail.com>
2023-07-25 16:29:51 +0300

    task 1

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,5 @@
 #include "sort.h"
-
+void swap_integers(int *a, int *b);
 /**
  * bubble_sort - bubble sort function
  * @array: input array pointer
@@ -7,8 +7,43 @@
  */
 void bubble_sort(int *array, size_t size)
 {
+	size_t i = 0, j = 0;
+	int swapped = 1;
+
+	if (!array || size < 2)
+		return;
+	while (swapped != 0)
+	{
+		swapped = 0;
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap_integers(array + j, array + (j + 1));
+				print_array((const int *)array, size);
+				swapped = 1;
+			}
+		}
+		i++;
+		if (swapped == 0)
+			break;
+	}
 
 
 
+}
 
+
+/**
+ * swap_integers - function that swaps two integers
+ * @a: First integer
+ * @b: Second integer
+ */
+void swap_integers(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
 }
